520f9b0e6bc9762c7b05aca3685bb71050514814
vishnuprasaath <vishnuprasaath2005@gmail.com>
2024-08-21 19:40:29 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("welcome to my ai app!")
+st.header("chat bot!!!! ")
+st.subheader("vishnuprasaath")
+
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBoPr95TzPs1bOTBamWMajCPBjl-FbdTUE")
+
+
+text=st.text_input("enter your question:")
+
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+
+
+if st.button("Click me"):
+    response = chat.send_message(text)
+    st.write(response.text)
+    
+
+
+
